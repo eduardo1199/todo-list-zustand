42188126cc802a22ec2db38090129a1aba57c97f
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-06 12:34:25 -0300

    reference ids task in HTML form

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,6 +1,7 @@
 import * as CheckBox from "@radix-ui/react-checkbox"
 
 import { CheckIcon  } from '@radix-ui/react-icons'
+import React from "react";
 
 import useTaskStore from '../../store/task-slice';
 interface TaskProps {
@@ -17,22 +18,20 @@ export function Task({ checked, label, id }: TaskProps) {
   }
 
   return (
-    <div className="flex gap-3 items-center">
-      <CheckBox.Root 
-        id="checkbox-1" 
-        className="flex items-center gap-3 group" 
-        checked={checked} 
-        onCheckedChange={() => toggleCheckTask(id)}
-      >
-        <div className="h-6 w-6 rounded-lg flex items-center justify-center border-2 border-white group-data-[state=checked]:bg-rose-500 group-data-[state=checked]:border-rose-500 transition-colors group-focus:ring-2 group-focus:ring-rose-500 group-focus:ring-offset-2 group-focus:ring-offset-background">
-          <CheckBox.CheckboxIndicator>
-            <CheckIcon />
-          </CheckBox.CheckboxIndicator>
-        </div>
-        <label htmlFor="checkbox-1" className="font-bold text-lg group-data-[state=checked]:line-through group-data-[state=checked]:opacity-70">
-          {label}
-        </label>
-      </CheckBox.Root>
-    </div>
+    <CheckBox.Root 
+      id={`checkbox-${id}`}
+      className="flex items-center gap-3 group" 
+      checked={checked} 
+      onCheckedChange={() => toggleCheckTask(id)}
+    >
+      <div className="h-6 w-6 rounded-lg flex items-center justify-center border-2 border-white group-data-[state=checked]:bg-rose-500 group-data-[state=checked]:border-rose-500 transition-colors group-focus:ring-2 group-focus:ring-rose-500 group-focus:ring-offset-2 group-focus:ring-offset-background">
+        <CheckBox.CheckboxIndicator>
+          <CheckIcon />
+        </CheckBox.CheckboxIndicator>
+      </div>
+      <label htmlFor={`checkbox-${id}`} className="font-bold text-lg group-data-[state=checked]:line-through group-data-[state=checked]:opacity-70">
+        {label}
+      </label>
+    </CheckBox.Root>
   )
 }
